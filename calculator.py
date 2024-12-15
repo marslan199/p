b76a89a1ceba7d4c5a3c20a5a8b4231ad4791a02
M ARSLAN <mianarslan19900@gmail.com>
2024-12-15 22:47:24 +0500

    Completed implementation

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a=45
+b=7
+ans1=a+b
+print("Addition of ",a ,"and ", b ,"is ", a+b)
+print("Subtraction of ",a ,"and ", b ,"is ", a-b)
+print("Multiplication of ",a ,"and ", b ,"is ", a*b)
+print("Division of ",a ,"and ", b ,"is ", a/b)
+print("Modulus of ",a, "and ", b ,"is ", a%b)
+print("Power of ",a ,"and ", b ,"is ", a**b)
