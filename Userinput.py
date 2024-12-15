585fb91f6270b86506e6886c5d785ef063677463
M ARSLAN <mianarslan19900@gmail.com>
2024-12-15 22:59:00 +0500

    User Input

---
+++ Userinput.py
@@ -0,0 +1,8 @@
+# a=input("enter name ")
+# print(a)
+num1=input("Enter number 1 : ")
+num2=input("Enter number 2 : ")
+# here it would concatenate these number by take them as strings
+print(num1+num2)
+# so we have to type cast
+print(int(num1)+int(num2))
