49800b04f11184f527eba6505358cb40f36a56ea
M ARSLAN <mianarslan19900@gmail.com>
2024-12-15 22:37:01 +0500

    first day

---
+++ pip.py
@@ -0,0 +1,10 @@
+print("hell")
+a=10
+print("this is number    ",a)
+# ...............................
+# seprating character
+print("arslan","mian","asif", sep="?")
+# type checking
+print(type(a))
+listl1=[1,2,3,4,5]
+print(listl1)
