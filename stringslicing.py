c17956e29ab123a4cb8c026de295430389c31dad
M ARSLAN <mianarslan19900@gmail.com>
2024-12-16 23:44:29 +0500

    activity

--- stringslicing.py
+++ stringslicing.py
@@ -5,3 +5,6 @@ print(name[3:7])
 print(name[3:-6])
 # here first is subtract 4 from total index and start from that index and move to next where we subtract 1 from total 
 print(name[-4:-1])
+# activity
+nm="harry"
+print(nm[-4:-2])
