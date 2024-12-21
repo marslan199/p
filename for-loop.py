a62c2de7c95dc75dbd4b3a842073a38cfa26bf79
M ARSLAN <mianarslan19900@gmail.com>
2024-12-21 23:21:37 +0500

    for num array

--- for-loop.py
+++ for-loop.py
@@ -1,3 +1,11 @@
 a="arslan"
 for i in a:
  print(i)
+#  to print eliments in list
+list=["arslan","raza","imran"]
+for i in list:
+ print(i)
+#  to print array of numbers to certain range
+
+for nums in range(10):
+ print(nums)
