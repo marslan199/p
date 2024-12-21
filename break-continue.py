554f9954d1244517ec919962c222351d6e2188f1
M ARSLAN <mianarslan19900@gmail.com>
2024-12-21 23:59:14 +0500

    break or continue

---
+++ break-continue.py
@@ -0,0 +1,13 @@
+for i in range(13):
+  
+    if(i==10):
+     print ("hell")
+     continue
+    print("5 X",i,"=",5*i)
+    
+for i in range(13):
+  
+     if(i==10):
+      
+      break
+     print("5 X",i,"=",5*i)
