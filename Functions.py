0b37deceb3c2e7cfec02101dcf3fb8e5aa6f498b
M ARSLAN <mianarslan19900@gmail.com>
2024-12-24 23:29:02 +0500

    function types

--- Functions.py
+++ Functions.py
@@ -1,6 +1,9 @@
+
+# parametrized function 
 def calculate(a,b):
     gmean=(a*b)+(a**2)
     print(gmean)
+# unparametrized function
 def isgreater():
     a=2
     b=2345
@@ -8,10 +11,25 @@ def isgreater():
         print(a," is gretaer ")
     else:
         print(b," is greater")
+
+def islesser(a,b):
+    # if you want that you initializa body of function later use keyword 
+ pass
+# function with default values 
+def default(a=2,b=13):
+   print(a,b)
+def nums(*number): # *numbers provide a kind of list 
+   sum=0
+   for i in number:
+      sum=sum+i
+   return sum
 a=10
 b=20
 calculate(a,b)
 isgreater()
-def islesser(a,b):
-    # if you want that you initializa body of function later use keyword 
- pass
+default( b=10) #here it take default a value
+default( ) # here both are default
+default( 12) # here b has default value '
+z = nums(4,5,9,2)
+print("is ans sjjnd",z)
+
