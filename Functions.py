52b07c8264badca18614ef57eed031059e6c140c
M ARSLAN <mianarslan19900@gmail.com>
2024-12-24 23:12:23 +0500

    pass keyword

--- Functions.py
+++ Functions.py
@@ -1,7 +1,17 @@
 def calculate(a,b):
     gmean=(a*b)+(a**2)
     print(gmean)
-
+def isgreater():
+    a=2
+    b=2345
+    if(a>b):
+        print(a," is gretaer ")
+    else:
+        print(b," is greater")
 a=10
 b=20
 calculate(a,b)
+isgreater()
+def islesser(a,b):
+    # if you want that you initializa body of function later use keyword 
+ pass
